f476a75d5bec8d81c2bd5b1d4e521570fea84bd0
hogstech <hogstech98@gmail.com>
2025-11-18 20:03:18 -0500

    Updating member profiles
    - Moved old member photos to the old-data folder
    - Added new member photos to the folder
    - Updated profiles.js to have the new member profiles
      - Also added a new thing to the js file so now if there isn't a "full" size member picture there isn't just a big blank space at the top of the popup
    - Updated a bit of the index.css so the overlays when hovering over images are centered on the image
    - Updated a part in index.html so a couple models are formatted correctly

--- js/profiles.js
+++ js/profiles.js
@@ -74,250 +74,157 @@ const memberData = {
 			"bio": "My name is Jack Fontaine, and I am a third year mechanical engineering student from northern Virginia. I like nature, games, and playing hockey. I look forward to getting to know new members- do not hesitate to reach out if you want to talk or have questions!"
 		},
 
-
-
-		{   "name": "Dwayne the Rock",
-			"major": "Door Holding",
-			"enrollment": "Beyond Time",
+		// Non E-Board member section starts here
+		{   "name": "Lily Fagan",
+			"major": "Computational Neuroscience",
+			"enrollment": "First Year",
 			"photos": [
 				{
-					"URL": "images/memes/dwayne_the_rock.jpg",
+					"URL": "images/members/lily_fagan.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/memes/dwayne_the_rock_fun.jpg",
+					"URL": "images/members/lily_fagan_fun.jpg",
 					"size": "full"
 				}
 			],
-			"bio": "Hey there! I'm Dwayne, the Rock, and I'm always happy to hold the T-Lounge door open for you!\nhttps://www.youtube.com/shorts/eDTnhSUekKw"
+			"bio": "Heyo! I’m Lily Fagan, a 1st year computational neuroscience major from CT. When I’m not at fencing club practice, you can find me skating around campus or playing Dungeons and Dragons."
 		},
-		{   "name": "Big Red",
-			"major": "Electrodynamics",
-			"enrollment": "Time is relative",
+		{   "name": "Lex Fogg",
+			"major": "Chemistry",
+			"enrollment": "First Year",
 			"photos": [
 				{
-					"URL": "images/memes/big_red.jpg",
+					"URL": "images/members/lex_fogg.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/memes/big_red_fun.jpg",
+					"URL": "images/members/lex_fogg_fun.jpg",
 					"size": "full"
 				}
 			],
-			"bio": "Hello! I'm Big Red and I like long cables like myself to bring around angry pixies! I live in H.E.L.L, don't don't visit me I don't like people."
+			"bio": "I'm Lex! I'm an on-floor member of HoGS and I love nature. I'm from the Saint Louis area (a long drive, I know). I like crafting, quad skating, and DnD."
 		},
-		{   "name": "Virilicus",
-			"major": "Archeologist",
-			"enrollment": "Ancient",
+		{   "name": "Will Sionkiewicz",
+			"major": "Cybersecurity",
+			"enrollment": "First Year",
 			"photos": [
 				{
-					"URL": "images/memes/virilicus.jpg",
+					"URL": "images/members/will_sionkiewicz.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/memes/virilicus_fun.jpg",
+					"URL": "images/members/will_sionkiewicz_fun.jpg",
 					"size": "full"
 				}
 			],
-			"bio": "Gimme your kneecaps."
+			"bio": "Hi I'm Will, I didn't really have a specific interest in science until I met some people in HoGS and realized they were really cool, so I joined mid year. It's been a great community and I've had a lot of fun doing different activities with everyone."
 		},
-
-		{   "name": "Lucas Gordon",
-			"major": "Industrial Design",
-			"enrollment": "Third Year",
-			"photos": [
-				{
-					"URL": "images/members/lucas_gordon.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/lucas_gordon_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I have always been a maker. I've been an avid LEGO fan since the age of 4, and I always gravitated toward activities with creative outlets. I have also always been interested in science and technology and finding ways to use them to solve practical problems. After a bit of turbulence, I ended up in Industrial/Product Design and am now in my third year of the program, enjoying taking ideas from the conceptual stage all the way to production."
-		},
-		{   "name": "Joseph Gysbers",
-			"major": "Mechanical Engineering",
+		{   "name": "Matthew Freunscht",
+			"major": "Biology",
 			"enrollment": "Second Year",
 			"photos": [
 				{
-					"URL": "images/members/joseph_gysbers.jpg",
+					"URL": "images/members/matthew_freunscht.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/members/joseph_gysbers_fun.jpg",
+					"URL": "images/members/matthew_freunscht_fun.jpg",
 					"size": "full"
 				}
 			],
-			"bio": "Hello! I'm Joe, a second year mechanical engineering major from Newfane, New York. I have served as both Vice-President and Housing Head, and I always help out with projects for Rochester Maker Faire and Imagine RIT. I'm usually hanging out on floor whenever I'm not busy, so if you need anything just let me know or feel free to stop by!"
+			"bio": "Hello! I'm Matt and I'm a second year Biology major. I enjoy hosting games and events for HOGS members and I look forward to doing so for the foreseeable future! Come join us as a new member!"
 		},
-		{   "name": "Kevin Wu",
+		{   "name": "Liam Nevins",
+			"major": "Chemistry",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/members/liam_nevins.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/liam_nevins_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I'm the social committee head for the house of general science, I help coordinate and plan events for the special interest house. I also like to help other committees like project and housing."
+		},
+		{   "name": "Kathryn Roy",
+			"major": "Computer engineering technology",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/members/kathryn_roy.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/kathryn_roy_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I'm Kathryn I love computers and drumming. I do a lot of different stuff for hogs and love going on trips for hogs and such."
+		},
+		{   "name": "Kenny Yang",
+			"major": "Computing and Information Tech",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/members/kenny_yang.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/kenny_yang_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I'm Kenny, the coffee and tea addict that works in CIT. I'm hoping to get a cool summer job this year in CIT to further my work! I like Pokemon, Minecraft and various Nintendo and Steam games. I like animation, drawing, electric music, Dungeons and Dragons, and math! My favorite food is Steak and Potatoes."
+		},
+		{   "name": "Henry B",
+			"major": "Biotechnology",
+			"minor": "Music Technology",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/members/henry_b.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/henry_b_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Sci-fi and science nerd!"
+		},
+		{   "name": "Eli Nolan",
+			"major": "Biology",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/members/eli_nolan.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/eli_nolan_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "My name is Eli and I am a second year biology major. I am co-head of Project Committee and People In Suits Subcommittee. We are always looking for new committee members, feel free to check out Project Committee in H.E.L.L. and People In Suits Subcommittee in [REDACTED]."
+		},
+		{   "name": "Matthew Gookin",
 			"major": "Computer Science",
 			"enrollment": "Second Year",
 			"photos": [
 				{
-					"URL": "images/members/kevin_wu.jpg",
+					"URL": "images/members/matthew_gookin.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/members/kevin_wu_fun.jpg",
+					"URL": "images/members/matthew_gookin_fun.jpg",
 					"size": "full"
 				}
 			],
-			"bio": "I'm from NYC. I love editing videos and coding. I'm responsible for some of the HoGS YouTube videos and server management for some of our games, mainly datapacks for one of our Minecraft servers. I love music composed by Hiroyuki Sawano, if anyone knows who that is."
-		},
-		{   "name": "Damian Suarez",
-			"major": "Mechanical Engineering Technology",
-			"enrollment": "Second Year",
-			"photos": [
-				{
-					"URL": "images/members/damian_suarez.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/damian_suarez_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I'm Damian Suarez, the man who makes things. I'm from Miami, Florida (Yes its hot, Yes we have mosquitos, Yes I know Mr. Worldwide) I joined HoGS in 2021, and briefly served as project manager. If they're is any random electrical parts or wires on floor Probably from me. When I'm not in class I like to hanging and chilling around floor, or blowing myself up, its a 50 50. Ill leave you with some advice \"You can't do two illegal things at once\"."
-		},
-		{   "name": "Leah Robinson",
-			"major": "Biotechnology & Molecular Bioscience",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/leah_robinson.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/leah_robinson_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Hello! I am an Accelerated Scholar and Performing Arts Scholar from Massachusetts."
-		},
-		{   "name": "Marah Lewis",
-			"major": "Physics",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/marah_lewis.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/marah_lewis_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Hello, my name's Marah (she/they/he) and I'm from Silver Spring, MD. I love anime, field hockey, reading and writing. If I seem really quiet and stand offish, I'm not being mean, I just have crippling social anxiety."
-		},
-		{   "name": "Daniel Lekan-Salami",
-			"major": "Mechatronics Engineering Technology",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/daniel_lekan_salami.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/daniel_lekan_salami_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I am a person. I eat food. Everything else you will learn from me in person."
-		},
-		{   "name": "Anthony Patti",
-			"major": "Biochemistry",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/anthony_patti.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/anthony_patti_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I'm a first year biochem major from Long island New York."
-		},
-		{   "name": "Katie Miller",
-			"major": "Chemistry",
-			"minor": "Dance",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/katie_miller.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/katie_miller_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Hello! My name is Katie Miller and I'm a first year chemistry major from Eagan, Minnesota. I'm also cross-registered student from NTID! Outside of school, I love to dance (ballet and tap are my favorites), do Taekwondo, play the violin, and arrange/compose music."
-		},
-		{   "name": "Nicolas Sovare",
-			"major": "Physics, Game Design & Development ",
-			"minor": "Math",
-			"enrollment": "Second Year",
-			"photos": [
-				{
-					"URL": "images/members/nicolas_sovare.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/nicolas_sovare_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Physics Major at RIT. Everyone thinks I'm insane and they're probably right. I have three younger brothers who are, unfortunately, not physics majors at RIT"
-		},
-		{   "name": "Robert Vazquez",
-			"major": "Civil Engineering Technology",
-			"enrollment": "Second Year",
-			"photos": [
-				{
-					"URL": "images/members/robert_vazquez.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/robert_vazquez_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "My name is Robert Anthony Vazquez. I am a second year Civil Engineering Technology (CVET) student. I'm a martial artist who's always interested in learning new things."
-		},
-		{   "name": "Benny Wang",
-			"major": "Applied Statistics",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/benny_wang.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/benny_wang_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Benny's never gonna give up dadjoking you, and he's never gonna give up just brute forcing math problems. He's never gonna let you live down your most embarrassing moment, and he's never gonna run around campus playing Pokemon go. That's a lie, he'll desert you to go catch that gible. If it wasn't obvious, Benny loves his rickrolls. He plays valorant and pokemon go, and enjoys listening to Chinese music."
-		},
-		{   "name": "Ryan Armitage",
-			"major": "Applied Mathematics",
-			"enrollment": "First Year",
-			"photos": [
-				{
-					"URL": "images/members/ryan_armitage.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/ryan_armitage_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I like warlocks, wheely chairs, and wooing ladies. I enjoy frolicking in various fields such as wheat, corn and soy. I live my life on the edge."
+			"bio": "I'm Matthew, a second year computer science major and the head of Tech Committee for this year. I like reading (mainly sci-fi and fantasy), playing video games, and basically anything tech-related, so feel free to talk to me about any of those."
 		}
 	]
 }
@@ -330,6 +237,21 @@ let imageFailListener = {
 	listener: (event) => {
 		if (event.target instanceof HTMLImageElement) {
 			event.target.setAttribute("src", "images/unknownProfile.svg");
+		} else {
+			throw "imageFail called on something other than an image";
+		}
+	}
+};
+
+let imageFailListenerMemberPopup = {
+	type: "error",
+	once: true,
+	passive: true,
+	capture: false,
+	listener: (event) => {
+		if (event.target instanceof HTMLImageElement) {
+			event.target.setAttribute("src", "images/unknownProfile.svg");
+			event.target.setAttribute("height", "0");
 		} else {
 			throw "imageFail called on something other than an image";
 		}
@@ -429,7 +351,7 @@ processMembers: {
 	modals = members.map(member =>
 		new Elmen("div").withClasses("popup-modal", "mfp-hide").withAttributes({id: `modal-${member.id}`}).withChildren(
 			new Elmen("div").withClasses("media").withChildren(
-				new Elmen("img").withAttributes({src: member.photos.find(photo => photo.size == "full").URL, alt: (member.name ? member.name : "")}).withListeners(imageFailListener)
+				new Elmen("img").withAttributes({src: member.photos.find(photo => photo.size == "full").URL, alt: (member.name ? member.name : "")}).withListeners(imageFailListenerMemberPopup)
 			),
 			new Elmen("div").withClasses("description-box").withChildren(
 				new Elmen("h4").withChildren(member.name),
